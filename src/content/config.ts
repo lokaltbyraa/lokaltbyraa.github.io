105494efc4eabac78c05b19c7c921177197a49e7
lokaltbyraa <172643970+lokaltbyraa@users.noreply.github.com>
2024-07-31 23:12:48 +0200

    Started setting up ansatt data collection. Trying to solve how to insert ansatt data into kontaktperson.data used in slug.astro

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,22 +1,59 @@
 
+import { defineCollection, z, reference } from 'astro:content';
 
-import { defineCollection, z } from 'astro:content';
+// const projectsCollection = defineCollection({
+//     type: 'content',
+//     schema: z.object({
+//       tittel: z.string(),
+//       sammendrag: z.string(),
+//       mainImage: z.string(),
+//       altText: z.string(),
+//       oppdragsgiver: z.array(z.string()),
+//       samarbeidspartner: z.array(z.string()),
+//       leveranser: z.array(z.string()),
+//       tidspunkt: z.string(),
+//       tags: z.array(z.string()),
+//       rekkefølge: z.number(),
+//       vårkontaktperson: z.array(reference("projectsCollection")),
+//     }),
+//   });
+
+// export const collections = {
+//   prosjekter: projectsCollection,
+// };
+
+
 const projectsCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-      tittel: z.string(),
-      sammendrag: z.string(),
-      mainImage: z.string(),
+  type: 'content',
+  schema: z.object({
+    tittel: z.string(),
+    sammendrag: z.string(),
+    mainImage: z.string(),
+    altText: z.string(),
+    oppdragsgiver: z.array(z.string()),
+    samarbeidspartner: z.array(z.string()),
+    leveranser: z.array(z.string()),
+    tidspunkt: z.string(),
+    tags: z.array(z.string()),
+    rekkefølge: z.number(),
+    vårKontaktperson: reference('ansatte'),
+  })
+});
+
+const ansatte = defineCollection({
+  schema: z.object({
+    navn: z.string(),
+    stilling: z.string(),
+    epost: z.string(),
+    telefon: z.number(),
+    headshot: z.object({
+      src: z.string(),
       altText: z.string(),
-      oppdragsgiver: z.array(z.string()),
-      samarbeidspartner: z.array(z.string()),
-      leveranser: z.array(z.string()),
-      tidspunkt: z.string(),
-      tags: z.array(z.string()),
-      rekkefølge: z.number(),
     }),
-  });
+  })
+});
 
 export const collections = {
-  prosjekter: projectsCollection,
+  'prosjekter': projectsCollection,
+  'ansatte': ansatte,
 };
