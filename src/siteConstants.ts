ca308fa2f4e79f8699b5b9b229611aeb013f1824
lokaltbyraa <172643970+lokaltbyraa@users.noreply.github.com>
2024-08-15 22:10:06 +0200

    Added favicon files, dynamic site titles, descriptions andnauthor, and meta tag stopping ios from recognizing phone numbers

--- src/siteConstants.ts
+++ src/siteConstants.ts
@@ -1,4 +1,5 @@
 export const SITE_TITLE = 'Lokalt Byrå';
+export const SITE_AUTHOR = 'Lokalt Byrå';
 export const SITE_DESCRIPTION =
   'Strategisk design for bærekraftige lokalsamfunn';
 export const HOMEPAGE_PROJECTS_LIMIT = 6;
